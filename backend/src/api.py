a9e62395af040b784c9cfabc3297af72cc63ec37
ZafarNishonov <nishonovofficial@gmail.com>
2021-09-19 19:18:47 +0500

    first commit

---
+++ backend/src/api.py
@@ -0,0 +1,350 @@
+import os
+from flask import Flask, request, jsonify, abort
+import json
+from flask_cors import CORS
+
+from .database.models import setup_db, Actors, Movies
+from .auth.auth import AuthError, requires_auth
+
+def create_app(test_config=None):
+
+    app = Flask(__name__)
+    setup_db(app)
+    CORS(app, resources={r"/api/*": {"origins": "*"}})
+
+    @app.after_request
+    def after_request(response):
+        response.headers.add('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+        response.headers.add('Access-Control-Allow-Headers', 'GET, POST, PATCH, DELETE, OPTIONS')
+        return response
+
+    @app.route('/', methods=['GET'])
+    def index():
+        return "Casting Agency API is working"
+
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+#                          Endpoints for movies                        #
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+
+
+    @app.route('/actors', methods=['GET'])
+    def show_actors():
+        query_actors = Actors.query.all()
+
+        if query_actors is None:
+            abort(404)
+
+        actors = [actors.short() for actors in query_actors]
+        return jsonify({
+            'success': True,
+            'actors': actors
+            })
+
+
+    @app.route('/actors/<int:id>', methods=['GET'])
+    def show_actor():
+        query_actor = Actors.query.filter_by(Actors.id == id).one_or_none()
+        actor = [actor.short() for actor in query_actor]
+        return jsonify({
+            'success': True,
+            'actor_info': actor
+            }), 200
+
+
+    @app.route('/actors-detail', methods=['GET'])
+    @requires_auth('get:actors-detail')
+    def actors_detail(payload):
+        query_actors = Actors.query.all()
+
+        if query_actors is None:
+            abort(404)
+
+        actors = [actors.long() for actors in query_actors]
+        return jsonify({
+            'success': True,
+            'actors_detail': actors
+            })
+
+    
+    @app.route('/actors/<int:id>/detail', methods=['GET'])
+    @requires_auth('get:actors/<id>/detail')
+    def actor_detail(payload, id):
+        query_actor = Actors.query.filter_by(Actors.id == id).one_or_none()
+
+        if query_actor is None:
+            abort(404)
+
+        actor = [actor.long() for actor in query_actor]
+        return jsonify({
+            'success': True,
+            'actor_detail': actor
+            })
+
+    @app.route('/actors', methods=['POST'])
+    @requires_auth('post:actors')
+    def create_actors(payload):
+        body = request.get_json()
+        try:
+            name = body.get('name')
+            age = body.get('age')
+            gender = body.get('gender')
+            birth_day = body.get('birth_day')
+            birth_place = body,get('birth_place')
+
+            new_actor = Actors(name=name, age=age, gender=gender, birth_day=birth_day, birth_place=birth_place)
+
+            new_actor.insert()
+
+            actor = [new_actor.short()]
+            return jsonify({
+                'success': True,
+                'new_actor': actor
+                })
+
+        except:
+            abort(422)
+
+    @app.route('/actors<int:id>', methods=['PATCH'])
+    @requires_auth('patch:actors')
+    def edit_actor(payload, id):
+        query_actor = Actors.query.filter_by(Actors.id == id).one_or_none()
+        body = request.get_json()
+
+        if query_actor is None:
+            abort(404)
+
+        try:
+            name = body.get('name')
+            age = body.get('age')
+            gender = body.get('gender')
+            birth_day = body.get('birth_day')
+            birth_place = body,get('birth_place')
+
+            edited_actor = Actors(name=name, age=age, gender=gender, birth_day=birth_day, birth_place=birth_place)
+
+            edited_actor.update()
+
+            actor = [edited_actor.short()]
+            return ({
+                'success': True,
+                'edited_actor': actor
+                })
+
+        except:
+            abort(422)
+
+    @app.route('/actors/<int:id>', methods=['DELETE'])
+    @requires_auth('delete:actors')
+    def delete_actor(payload, id):
+        query_actor = Actors.query.filter_by(Actors.id == id).one_or_none()
+
+        if query_actor is None:
+            abort(404)
+
+        try:
+            query_actor.delete()
+
+            actor = [query_actor.short()]
+
+            return jsonify ({
+                'success': True,
+                'deleted_actor': actor
+                })
+
+
+        except:
+            abort(422)
+
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+#                          Endpoints for movies                        #
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+
+
+    @app.route('/movies', methods=['GET'])
+#    @requires_auth('get:movies')
+    def show_movies():
+        query_movies = Movies.query.all()
+
+        if query_movies is None:
+            abort(404)
+
+        movies = [movies.short() for movies in query_movies]
+        return jsonify({
+            'success': True,
+            'movies': movies
+            })
+
+
+    @app.route('/movies/<int:id>', methods=['GET'])
+#    @requires_auth('get:movies/<id>')
+    def show_movie():
+        query_movie = Movies.query.filter_by(Movies.id == id).one_or_none()
+
+        if query_movie is None:
+            abort(404)
+
+        movie = [movie.short() for movie in query_movie]
+        return jsonify({
+            'success': True,
+            'movie_info': movie
+            })
+
+
+    @app.route('/movies-detail', methods=['GET'])
+    @requires_auth('get:movies-detail')
+    def movies_detail(payload):
+        query_movies = Movies.query.all()
+
+        if query_movies is None:
+            abort(404)
+
+        movies = [movies.long() for movies in query_movies]
+        return jsonify({
+            'success': True,
+            'movies_detail': movies
+            })
+
+    
+    @app.route('/movies/<int:id>/detail', methods=['GET'])
+    @requires_auth('get:movies/<id>/detail')
+    def movie_detail(payload, id):
+        query_movie = Movies.query.filter_by(Movies.id == id).one_or_none()
+
+        if query_movie is None:
+            abort(404)
+
+        movie = [movie.long() for movie in query_movie]
+        return jsonify({
+            'success': True,
+            'movie_detail': movie
+            })
+
+    @app.route('/movies', methods=['POST'])
+    @requires_auth('post:movies')
+    def create_movies(payload):
+        body = request.get_json()
+        try:
+            title = body.get('title')
+            release_date = body.get('release_date')
+            budget = body.get('budget')
+            producer = body.get('producer')
+            genres = body,get('genres')
+
+            new_movie = Movies(title=title, release_date=release_date, budget=budget, producer=producer, genres=genres)
+
+            new_movie.insert()
+
+            movie = [new_movie.short()]
+            return jsonify({
+                'success': True,
+                'new_movie': movie
+                })
+
+        except:
+            abort(422)
+
+    @app.route('/movies<int:id>', methods=['PATCH'])
+    @requires_auth('patch:movies')
+    def edit_movie(payload, id):
+        query_movie = Movies.query.filter_by(Movies.id == id).one_or_none()
+        body = request.get_json()
+
+        if query_movie is None:
+            abort(404)
+
+        try:
+            title = body.get('title')
+            release_date = body.get('release_date')
+            budget = body.get('budget')
+            producer = body.get('producer')
+            genres = body,get('genres')
+
+            edited_movie = Movies(title=title, release_date=release_date, budget=budget, producer=producer, genres=genres)
+
+            edited_movie.update()
+
+            movie = [edited_movie.short()]
+            return ({
+                'success': True,
+                'edited_movie': movie
+                })
+
+        except:
+            abort(422)
+
+    @app.route('/movies/<int:id>', methods=['DELETE'])
+    @requires_auth('delete:movies')
+    def delete_movie(payload, id):
+        query_movie = Movies.query.filter_by(Movies.id == id).one_or_none()
+
+        if query_movie is None:
+            abort(404)
+
+        try:
+            query_movie.delete()
+
+            movie = [query_movie.short()]
+
+            return jsonify ({
+                'success': True,
+                'deleted_movie': movie
+                })
+
+
+        except:
+            abort(422)
+
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+#                          Errorhandlers                               #
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+#  unprocessable                   #
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+
+
+    @app.errorhandler(422)
+    def unprocessable(error):
+        return jsonify({
+            "success": False,
+            "error": 422,
+            "message": "unprocessable"
+        }), 422
+    
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+#  not found                       #
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+    
+    
+    @app.errorhandler(404)
+    def not_found(error):
+        return jsonify({
+            "success": False,
+            "error": 404,
+            "message": "not found"
+        }), 404
+    
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+#  unauthorized                    #
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+    
+    
+    @app.errorhandler(AuthError)
+    def unauthorized(error):
+        return jsonify({
+            "success": False,
+            "error": error.status_code,
+            "message": error.error
+        }), error.status_code
+
+
+    return app
+
+app = create_app()
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
---
+++ backend/src/database/models.py
@@ -0,0 +1,113 @@
+import os 
+from flask_sqlalchemy import SQLAlchemy
+import json
+
+database_path = os.getenv("DATABASE_URI")
+
+db = SQLAlchemy()
+
+
+def setup_db(app, database_path=database_path):
+    app.config["SQLALCHEMY_DATABASE_URI"] = database_path
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.app = app
+    db.init_app(app)
+
+
+association_table = db.Table('casting_agency',
+    db.Column('actor_id', db.Integer, db.ForeignKey('actors.id'), primary_key=True),
+    db.Column('movie_id', db.Integer, db.ForeignKey('movies.id'), primary_key=True)
+)
+
+
+class Actors(db.Model):  
+  __tablename__ = 'actors'
+  id = db.Column(db.Integer, primary_key=True)
+  name = db.Column(db.String())
+  age = db.Column(db.Integer)
+  gender = db.Column(db.String(10))
+  birth_day = db.Column(db.String(50))
+  birth_place = db.Column(db.String(50))
+
+  def __init__(self, name, age, gender):
+    self.name = name
+    self.age = age 
+    self.gender = gender
+    self.birth_day = birth_day
+    self.birth_place = birth_place
+
+  def short(self):
+    return {
+    'id': self.id,
+    'name': self.name,
+    'gender': self.gender,
+    }
+  
+  def long(self):
+    return {
+    'id': self.id,
+    'name': self.name,
+    'age': self.age,
+    'gender': self.gender,
+    'birth_day': self.birth_day,
+    'birth_place': self.birth_place,
+    'movies': [movie.short() for movie in self.movies]
+    }
+
+  def insert(self):
+    db.session.add(self)
+    db.session.commit()
+
+  def update(self):
+    db.session.commit()
+
+  def delete(self):
+    db.session.delete(self)
+    db.session.commit()
+
+
+class Movies(db.Model):  
+  __tablename__ = 'movies'
+  id = db.Column(db.Integer, primary_key=True)
+  title = db.Column(db.String(), nullable=False)
+  release_date = db.Column(db.String(), nullable=False)
+  budget = db.Column(db.String(), nullable=False)
+  producer = db.Column(db.String(), nullable=False)
+  genres = db.Column(db.String(), nullable=False)
+  actors = db.relationship('Actors', secondary=association_table, backref=db.backref('movies', lazy=True))
+
+  def __init__(self, title, release_date, budget, producer):
+    self.title = title
+    self.release_date = release_date
+    self.budget = budget
+    self.producer = producer
+    self.genres = genres
+
+  def short(self):
+    return {
+    'id': self.id,
+    'title': self.title,
+    'release_date': self.release_date
+    }
+
+  def long(self):
+    return {
+    'id': self.id,
+    'title': self.title,
+    'genres': self.genres,
+    'release_date': self.release_date,
+    'budget': self.budget,
+    'producer': self.producer,
+    'actors': [actor.short() for actor in self.actors]
+    }
+  
+  def insert(self):
+    db.session.add(self)
+    db.session.commit()
+
+  def update(self):
+    db.session.commit()
+
+  def delete(self):
+    db.session.delete(self)
+    db.session.commit()
