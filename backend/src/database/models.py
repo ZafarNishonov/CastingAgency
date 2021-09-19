8f173bf2dab450e994ef7757a860c79673b32488
ZafarNishonov <nishonovofficial@gmail.com>
2021-09-19 19:41:17 +0500

    commit

--- backend/src/database/models.py
+++ backend/src/database/models.py
@@ -12,6 +12,7 @@ def setup_db(app, database_path=database_path):
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.app = app
     db.init_app(app)
+    db.create_all()
 
 
 association_table = db.Table('casting_agency',
