ca46963ac508f32e7389c0bed116bf24a9d65363
ZafarNishonov <nishonovofficial@gmail.com>
2021-09-20 23:08:34 +0500

    updated readme

--- backend/src/api.py
+++ backend/src/api.py
@@ -20,7 +20,7 @@ def create_app(test_config=None):
 
     @app.route('/', methods=['GET'])
     def index():
-        return "Casting Agency API is working"
+        return "Finally! I have done it! Casting Agency API is working"
 
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
